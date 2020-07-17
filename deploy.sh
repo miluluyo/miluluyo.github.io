b5b7e744b2208ea73d650e939e2fa76a490aa648
miluluyo@163.com <miluluyo@163.com>
2020-07-17 12:06:49 +0800

    更改部署

--- deploy.sh
+++ deploy.sh
@@ -27,14 +27,14 @@ git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
 #echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:miluluu/miluluu.git
-else
-  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#  codingUrl=git@e.coding.net:miluluu/miluluu.git
+#else
+#  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
+#fi
+#git add -A
+#git commit -m "${msg}"
+#git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
